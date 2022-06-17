b9f1fe66871fd8f99af160b4214bd0246bc5c770
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-06-17 14:43:41 +0800

    开发

--- src/router/index.js
+++ src/router/index.js
@@ -3,34 +3,32 @@ import {
   createWebHashHistory
 } from 'vue-router';
 import Home from '../components/Home.vue';
-import Welcome from '../components/Welcome.vue';
-import Login from '../components/Login.vue';
 const routes = [{
-  name: 'home',
-  path: '/',
-  meta: {
-    title: '首页'
-  },
-  component: Home,
-  redirect: '/welcome',
-  children: [{
+    name: 'home',
+    path: '/',
+    meta: {
+      title: '首页'
+    },
+    component: Home,
+    redirect: '/welcome',
+    children: [{
       name: 'welcome',
       path: '/welcome',
       meta: {
         title: '欢迎页'
       },
-      component: Welcome
+      component:()=>import('../views/Welcome.vue')
+    }]
+  },
+  {
+    name: 'login',
+    path: '/login',
+    meta: {
+      title: '登录'
     },
-    {
-      name: 'login',
-      path: '/login',
-      meta: {
-        title: '登录'
-      },
-      component: Login
-    }
-  ]
-}]
+    component: ()=>import('../views/Login.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
